7fe62abb10870edeaa61c5a3d4cd324ca6d9ad46
sanghangning <hzshn664652740@gmail.com>
2023-04-26 10:26:28 +0800

    feat:index music player Web Audio

--- docs/.vitepress/theme/components/views/index/components/MusicPlayer.vue
+++ docs/.vitepress/theme/components/views/index/components/MusicPlayer.vue
@@ -1,6 +1,15 @@
 <template>
   <div class="music-wrap">
-    <div class="music-bar"></div>
+    <div class="music-bar">
+      <audio
+        id="globalAudio"
+        :src="active && active.href"
+        type="audio/mp3"
+        preload="meta"
+        controls
+      ></audio>
+      <canvas id="musicBar"></canvas>
+    </div>
     <div class="container">
       <div class="music-cd-wrap">
         <div class="music-cd">
@@ -22,7 +31,98 @@
   </div>
 </template>
 
-<script setup lang="ts"></script>
+<script setup lang="ts">
+  import { init } from 'vitepress'
+  import { onMounted, ref } from 'vue'
+  let canvas: HTMLCanvasElement | null
+  let audio: HTMLAudioElement | null
+  let ctx, dataArray, analyser
+  const active = ref<{
+    name: string
+    href: string
+  }>({
+    name: '兜圈',
+    href: 'https://cdn.chenyingshuang.cn/index/music/%E6%9E%97%E5%AE%A5%E5%98%89_%E5%85%9C%E5%9C%88.mp3',
+  })
+
+  onMounted(() => {
+    canvas = document.getElementById('musicBar') as HTMLCanvasElement
+    audio = document.getElementById('globalAudio') as HTMLAudioElement
+    ctx = canvas.getContext('2d')
+
+    initCanvas()
+    initAudio()
+  })
+
+  const initCanvas = () => {
+    //初始化canvas
+    if (!canvas) return
+    const wrap = document.querySelector('.music-bar') as HTMLDivElement
+    canvas.width = wrap!.clientWidth
+    canvas.height = wrap!.clientHeight
+  }
+
+  const initAudio = () => {
+    audio!.crossOrigin = 'anonymous'
+    //播放事件
+    audio!.onplay = function () {
+      //创建一个音频上下文实例
+      const audioCtx = new window.AudioContext()
+      //添加一个音频源节点
+      const source = audioCtx.createMediaElementSource(audio!)
+      //分析器节点
+      analyser = audioCtx.createAnalyser()
+      //fft分析器  越大 分析越细
+      analyser.fftSize = 512
+      //创建一个无符号字节的数组
+      dataArray = new Uint8Array(analyser.frequencyBinCount)
+      //音频源节点 链接分析器
+      source.connect(analyser)
+      //分析器链接输出设备
+      analyser.connect(audioCtx.destination)
+
+      draw()
+    }
+  }
+
+  const draw = () => {
+    requestAnimationFrame(draw)
+
+    const { width, height } = canvas!
+
+    ctx.clearRect(0, 0, width, height)
+    //分析器节点分析出的数据到数组中
+    ctx.fillStyle = '#78C5F7'
+    ctx.lineWidth = 2
+    ctx.beginPath()
+    //getByteFrequencyData，分析当前音频源的数据 装到dataArray数组中去
+    //获取实时数据
+    analyser.getByteFrequencyData(dataArray)
+    console.log(dataArray)
+    const len = dataArray.length
+    const barWidth = width / len
+    let x = 0
+    for (let i = 0; i < len; i++) {
+      const data = dataArray[i]
+      const barHeight = (data / 255) * height
+
+      // ctx.fillRect(x,y,barWidth,height)
+
+      let v = dataArray[i] / 128.0
+      let y = (v * height) / 2
+
+      if (i === 0) {
+        ctx.moveTo(x, y)
+      } else {
+        ctx.lineTo(x, y)
+      }
+
+      x += barWidth
+    }
+    // ctx.lineTo(canvas.width, canvas.height/2);
+    ctx.stroke()
+  }
+</script>
 
 <style scoped lang="less">
   .music-wrap {
@@ -32,6 +132,12 @@
     position: relative;
     background: #131313;
     .music-bar {
+      z-index: 1;
+      width: 100%;
+      height: 100%;
+      position: absolute;
+      top: 0;
+      left: 0;
     }
     .container {
       display: flex;
