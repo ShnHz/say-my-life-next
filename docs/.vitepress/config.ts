43022d94c55de514182df7107c934698f1f3b699
sanghangning <664652740@qq.com>
2023-05-15 21:58:37 +0800

    config:SSR

--- docs/.vitepress/config.ts
+++ docs/.vitepress/config.ts
@@ -1,4 +1,4 @@
-import path from 'path' 
+import path from 'path'
 
 import { defineConfig } from 'vitepress'
 import nav from './configs/nav'
@@ -7,7 +7,8 @@ import socialLinks from './configs/socialLinks'
 
 // https://vitepress.dev/reference/site-config
 export default defineConfig({
-  base:'/say-my-life-next/',
+  ssr: false,
+  base: '/say-my-life-next/',
   ignoreDeadLinks: true,
   title: '野宁新之助',
   description: 'Think twice before you do.',
@@ -53,6 +54,6 @@ export default defineConfig({
         '@': path.resolve(__dirname, '../../'),
         '@docs': path.resolve(__dirname, '../'),
       },
-    }
+    },
   },
 })
